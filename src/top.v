feef9a9e78fbaa1579324c0e3c5ddea8a1206d04
Johnny Cache <35432057+malefficient@users.noreply.github.com>
2020-10-31 19:44:26 -0400

    TODO: quarter clk

--- src/top.v
+++ src/top.v
@@ -1,37 +1,13 @@
 module top
 (
     input wire          clk,
-    input wire          ftdi_rx,
-    output wire         ftdi_tx,
-    input wire          board_rx,
-    output wire         board_tx,
-    output wire         board_rst,
-    output reg          led = 1'b1,
-    output wire [13:0]  debug_header
-);
-
-// Combinatorial logic
-assign board_tx = ftdi_rx;
-assign ftdi_tx = board_rx;
-
-wire rx_valid;
-wire [7:0] rx_data;
-
-uart_rx rxi (
-    .clk(clk),
-    .din(ftdi_rx),
-    .data_out(rx_data),
-    .valid(rx_valid)
-);
-
-reg rst_en;
-
-resetter rsti (
-    .clk(clk),
-    .rst(rst_en),
-    .rst_out(board_rst)
-);
-
+    output wire       dup_clk
+ );
+ 
+assign dup_clk = clk;
+// TODO: generate a quarter-speed clock and output to qrtr_clk
+// See https://www.beyond-circuits.com/wordpress/tutorial/tutorial1/ for relevant walkthrough
+/*
 always @(posedge clk)
 begin
     rst_en <= 1'b0;
@@ -40,5 +16,5 @@ begin
         rst_en <= 1'b1;
     end
 end
-
+*/
 endmodule
